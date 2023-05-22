bc81f95fd34f058dcbf6abd3670a7ee505466de0
kilimovaann <annkilimova85@gmail.com>
2023-05-22 21:34:56 +0100

    Task 1 is ready

--- HW3/Task1.py
+++ HW3/Task1.py
@@ -3,5 +3,16 @@
 # Слово та номер символу отримайте за допомогою input() або скористайтеся константою.
 # Наприклад (слово - "Python" а номер символу 3) - "The 3 symbol in 'Python' is 't' ".
 
-a = 'Python'
-print(a[0])  nnn
+word = input('Type a word: ')
+symbol = input(f'Type a symbol number from 0 to {len(word)-1}: ')
+if symbol.isdigit():
+    symbol = int(symbol)
+    if int(symbol) > len(word)-1:
+        print('Try again! The value exceeds the permissible value')
+    else:
+        print(f'The {symbol} symbol in {word} is {word[symbol]}')
+else:
+    print('Try again! You can type only a number!!!')
+
+
+
