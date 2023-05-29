0e9b242cec4e5c605c7c4fa286e0c5a8d7536900
kilimovaann <annkilimova85@gmail.com>
2023-05-29 10:28:45 +0300

    HW4 after corrections

--- HW4/Task2.py
+++ HW4/Task2.py
@@ -26,14 +26,14 @@ for key in shop_dict:
 # shop_dict = {'cito': 47.999, 'BB_studio': 42.999, 'momo': 49.999, 'main-service': 37.245, 'buy.now': 38.324,
 #              'x-store': 37.166, 'the_partner': 38.988, 'store': 37.720, 'rozetka': 38.003, 'test': 'test'}
 #
-# # for key in shop_dict:
-# #     try:
-# #         if shop_dict[key] > lower_limit and shop_dict[key] < upper_limit:
-# #             print(key)
-# #     except TypeError:
-# #             print('print_TypeError')
-# #     except Exception:
-# #             print('about all errors except KeyBoardInterrupt')
+# for key in shop_dict:
+#     try:
+#         if shop_dict[key] > lower_limit and shop_dict[key] < upper_limit:
+#             print(key)
+#     except TypeError:
+#             print('print_TypeError')
+#     except Exception:
+#             print('about all errors except KeyBoardInterrupt')
     # else:
     #     print('good')
     # finally:
